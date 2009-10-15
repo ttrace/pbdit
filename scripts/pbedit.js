7bb131f57fbcec7c33aad219bf1949787cb7cf9a
t_trace <taiyoster@gmail.com>
2009-10-15 11:23:04 +0900

    Solved image processing smoothness in operation.

--- scripts/pbedit.js
+++ scripts/pbedit.js
@@ -142,25 +142,45 @@ function blur()
 
 function color_change()
 {
+	document.color_progress = 0;
+	try
+	{
+		clearInterval(color_timer);
+	}
+	catch(e)
+	{
+		if(debug)window.console.log(e.message);
+	}
+
 	var preview_canvas =  document.getElementById("stored_preview");
 	var preview_ctx = preview_canvas.getContext("2d");
 	var preview_image_data = preview_ctx.getImageData( 0, 0, preview_canvas.width , preview_canvas.height);
 
 	preview_key = guid();
-	var current_key = preview_key + "";
+	var current_key = preview_key;
 	setTimeout( function(){ preview_process( 1 , preview_image_data) } , 1);
-	setTimeout( function(){ final_process_startar( current_key ) } , 800);
+	color_timer = setInterval( function(){ final_process_startar( current_key ) } , 100);
+//	window.console.log(current_key , preview_key , (current_key == preview_key));
 	return(false);
 }
 
 function final_process_startar ( current_key )
 {
-	if( current_key == preview_key )
+	var canvas =  document.getElementById("stored");
+	var ctx = canvas.getContext("2d");
+	var image_data = ctx.getImageData( 0, 0, canvas.width , canvas.height);
+
+	if( current_key == preview_key && document.color_progress < image_data.data.length )
 	{
-		var canvas =  document.getElementById("stored");
-		var ctx = canvas.getContext("2d");
-		var image_data = ctx.getImageData( 0, 0, canvas.width , canvas.height);
-		real_process( image_data , current_key );
+		if(!document.color_progress)document.color_progress = 0;
+		var start_offset = document.color_progress;
+		real_process( start_offset );
+		document.color_progress += 1000000;
+	}
+	else
+	{
+		clearInterval(color_timer);
+		document.color_progress = 0;
 	}
 	return( false );
 }
@@ -196,53 +216,46 @@ function preview_process( scale , preview_image_data )
 	}
 	
 	preview_ctx.putImageData(preview_image_data, 0, 0);
-
-//	following codes for block previewing
-
-// 	for( var xi = 0 ; xi < (image_data.width / scale) ; xi ++)
-// 	{
-// 		for( var yi = 0 ; yi < (image_data.height / scale) ; yi ++)
-// 		{
-// 			preArray = get_pixel_collor_array( (xi * scale), (yi * scale) , image_data );
-// 			preR = preArray[0];
-// 			preG = preArray[1];
-// 			preB = preArray[2];
-// 			preview_ctx.fillStyle = "rgb(" + preR + " , " + preG + " , " + preB + ")";
-// 			preview_ctx.fillRect ( (xi * scale), (yi * scale), scale, scale);
-// 		}
-// 	}
 }
 
-function real_process( image_data , current_key )
+//function real_process( image_data , start_offset )
+function real_process( start_offset )
 {
+//	var color_progress = document.color_progress;
 	var canvas =  document.getElementById("workspace");
 	var ctx = canvas.getContext("2d");
+	var image_data = ctx.getImageData( 0, 0, canvas.width , canvas.height);
+	if( start_offset == 0)
+	{
+		var initial_canvas =  document.getElementById("stored");
+		var initial_ctx = initial_canvas.getContext("2d");	
+		var image_data = initial_ctx.getImageData( 0, 0, canvas.width , canvas.height);
+	}
+
+	if(debug)window.console.log('progress' , start_offset, image_data.data.length);
 
 	var color_biass = color_expression();
-	var thread_length = 1000;
 	
-	for( var i = 0 ; ( i < image_data.data.length && preview_key == current_key) ; i += 4 )
+	for( var i = start_offset ; ( i < image_data.data.length && i < (start_offset + 1000000) ) ; i += 4 )
 	{
-		if( preview_key != current_key )
-		{
-			if(debug)window.console.log('Stopped', i, current_key, preview_key);
-			break;
-		}
 		image_data.data[i]     = index_color ("R" , real_color( "R" , image_data.data[ i ]    ) * color_biass[0] );
 		image_data.data[i + 1] = index_color ("G" , real_color( "G" , image_data.data[ i + 1 ]) * color_biass[1] );
 		image_data.data[i + 2] = index_color ("B" , real_color( "B" , image_data.data[ i + 2 ]) * color_biass[2] );
 	}
 	
-	var ctx = canvas.getContext("2d");
 	ctx.putImageData(image_data, 0, 0);
+
+	if( (start_offset + 1000000) > image_data.data.length )
+	{
 	var preview_canvas = document.getElementById("workspace_preview");
 		preview_canvas.style.width = "0px";
 		preview_canvas.style.height = "0px";
 
 	var image_export_link = document.getElementById('image_share');
 		image_export_link.href = canvas.toDataURL("image/png", "exported.png");
-
+		
 	removeClass(preview_canvas , 'previewing');
+	}
 }
 
 
